9fa3eff7ad649430edb9a00cf384dd8a2e7059f1
Maryam Kamel <maryamsaeed_mkamel@outlook.com>
2021-08-24 22:40:46 +0200

    last working robot with keyboard button

---
+++ Assets/Scripts/MacarinaDancer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MacarinaDancer : MonoBehaviour
+{
+    public AudioSource MacarenaSong;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MacarenaSong.Stop();
+    }
+
+  
+    public void OnAnimationStarts()
+    {
+        MacarenaSong.Play();
+    }
+    public void OnAnimationEnds()
+    {
+        MacarenaSong.Stop();
+    }
+}
---
+++ Assets/Scripts/RobotCOntroller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotCOntroller : MonoBehaviour
+{
+    private Rigidbody robotRigidBody;
+    public float forceFactor= 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        robotRigidBody = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        robotRigidBody.velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            //move forward
+            robotRigidBody.AddRelativeForce(transform.forward *forceFactor);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //move backword
+            robotRigidBody.AddForce(transform.forward * -forceFactor);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //move Left
+            robotRigidBody.AddForce(-forceFactor*transform.right);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //move Right
+            robotRigidBody.AddForce(forceFactor*transform.right);
+        }
+    }
+}
